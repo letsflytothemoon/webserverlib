03ec9cf99f0b3e3af64001690503fcfe4ad61622
Dmitriy L <losev3628@gmail.com>
2023-08-01 23:25:23 +1000

    no unicode, routing

--- webserver.h
+++ webserver.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <mutex>
 #include <queue>
 #include <functional>
+#include <utility>
 #include <boost/beast.hpp>
 #include <boost/algorithm/string.hpp>
 
@@ -43,206 +45,338 @@ namespace webserverlibhelpers
     {
         static const Needed& Result(const Needed& needed, const Nexts& ...)
         { return needed; }
-    };  
-}
-
-template <class CharT>
-struct webserverlib
-{
-    typedef std::basic_string<CharT> string;
-    typedef std::basic_ostringstream<CharT> ostringstream;
-    typedef webserverlibhelpers::CharTypeSpecific<CharT> CharTypeSpecific;
-    static constexpr std::basic_ostream<CharT> &out = CharTypeSpecific::out;
-    static constexpr std::basic_ostream<CharT> &err = CharTypeSpecific::err;
-    using BodyT = http::basic_dynamic_body<beast::basic_multi_buffer<std::allocator<CharT>>>;
-    using Request = http::request<BodyT>;
-    using Response = http::response<BodyT>;
+    };
 
     template <class Id>
     struct Prop
     {  
         typedef typename Id::ValueType ValueType;
-        ValueType _value;
+        ValueType value;
         Prop() {}
 
-        Prop(typename Id::ValueType value) : _value(value) { }
+        Prop(typename Id::ValueType value) : value(value) { }
 
-        Prop(Prop& other) : _value(other._value) { }
+        Prop(Prop& other) : value(other.value) { }
 
-        Prop(const Prop& other) : _value(other._value) { }
+        Prop(const Prop& other) : value(other.value) { }
 
         template <class T, class U, class ... Args>
         Prop(const T& t, const U& u, const Args& ... args) :
-        _value(webserverlibhelpers::Selector<Prop<Id>, T, U, Args ...>::Result(t, u, args ...)._value)
+        value(webserverlibhelpers::Selector<Prop<Id>, T, U, Args ...>::Result(t, u, args ...).value)
         { }
 
     };
-    
-    class HttpRequestContext
+}
+
+class NotFoundException : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    { return "404 - not found"; }
+};
+
+class BadRequestException : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    { return "bad request"; }
+};
+
+using Body     = http::dynamic_body;
+using Request  = http::request<Body>;
+using Response = http::response<Body>;
+
+class HttpRequestContext
+{
+public:
+    Request                 request;
+    std::queue<std::string> route;
+    std::stringstream       responseStream;
+
+    HttpRequestContext(Request request) :
+    request(std::move(request))
     {
-    protected:
-        Request            _request;
-        std::queue<string> _route;
-        ostringstream      _responseStream;
+        std::vector<std::string> path;
+
+        boost::split(path, request.target(), boost::is_any_of("/"));
+
+        for(auto i = ++path.begin(); i != path.end(); i++)
+            route.push(*i);
+    }
+
+    std::string GetPathStep()
+    {
+        std::string result(std::move(route.front()));
+        route.pop();
+        return std::move(result);
+    }
+};
+
+namespace routing
+{
+    class RouterEndPoint;
+
+    class Router
+    {
     public:
-        Request&            request        = _request;
-        std::queue<string>& route          = _route;
-        ostringstream&      responseStream = _responseStream;
+        virtual RouterEndPoint& GetEndPoint(HttpRequestContext&) = 0;
+        virtual ~Router() { }
+    };
 
-        HttpRequestContext(http::request<http::basic_dynamic_body<beast::basic_multi_buffer<std::allocator<CharT>>>> request) :
-        _request(std::move(request))
+    class RouterNode : public Router
+    {
+        std::map<std::string, Router*> routes;
+    public:
+        RouterNode(std::initializer_list<std::pair<const std::string, Router*>> init) :
+        routes(init)
+        { }
+
+        template <class ... Args>
+        RouterNode(Args&& ... args) :
+        routes(args ...)
+        { }
+
+        ~RouterNode()
         {
-            std::vector<std::basic_string<CharT>> path;
-
-            boost::split(path, request.target(), boost::is_any_of(CharTypeSpecific::slash));
-
-            for(auto i = ++path.begin(); i != path.end(); i++)
-                route.push(*i);
+            for(auto i = routes.begin(); i != routes.end(); i++)
+                delete i->second;
         }
 
-        string GetPathStep()
+        RouterEndPoint& GetEndPoint(HttpRequestContext& context) override
         {
-            string result(std::move(route.front()));
-            route.pop();
-            return result;
+            std::string pathStep = context.GetPathStep();
+            auto i = routes.find(pathStep);
+            if(i == routes.end())
+                throw NotFoundException();
+            return i->second->GetEndPoint(context);
         }
     };
 
-    class WebServer
+    class RouterEndPoint : public Router
     {
     public:
-        template <class ValueT>
-        struct ClassId
-        { typedef ValueT ValueType; };
+        RouterEndPoint& GetEndPoint(HttpRequestContext& context) override
+        { return *this; }
 
-        struct AddressId      : ClassId<std::string   > { };
-        struct PortId         : ClassId<unsigned short> { };
-        struct ThreadsCountId : ClassId<unsigned int  > { };
-        struct RequestProcId  :
-        ClassId<std::function<void(HttpRequestContext&)> > { };
+        virtual void ProcessRequest(HttpRequestContext&) = 0;
+    };
 
-        typedef Prop<AddressId     > Address;
-        typedef Prop<PortId        > Port;
-        typedef Prop<ThreadsCountId> Threads;
-        typedef Prop<RequestProcId > RequestProc;
-    protected:
-        typename Address    ::ValueType _address;
-        typename Port       ::ValueType _port;
-        typename Threads    ::ValueType _threads;
-        typename RequestProc::ValueType _requestProc;
+    class StaticDirectoryEndPoint : public RouterEndPoint
+    {
+        std::string dirName;
+    public:
+        StaticDirectoryEndPoint(std::string dirName) : dirName(std::move(dirName))
+        { }
 
-        std::queue<tcp::socket> _acceptedConnectionsQueue;
-        std::mutex _connectionsQueueMutex;
+        void ProcessRequest(HttpRequestContext& context) override
+        {
+            std::stringstream pathStringStream;
+            pathStringStream << dirName;
 
-        void ListenLoop()
+            while(!context.route.empty())
+            {
+
+                std::string pathStep = context.route.front();
+                context.route.pop();
+                if(pathStep == "..")
+                    throw BadRequestException();
+                pathStringStream << "/" << pathStep;
+            }
+
+            std::ifstream fstream(pathStringStream.str());
+            context.responseStream << fstream.rdbuf();
+        }
+    };
+
+    class StaticDocumentEndPoint : public RouterEndPoint
+    {
+        std::string fileName;
+    public:
+        StaticDocumentEndPoint(std::string fileName) : fileName(std::move(fileName))
+        { }
+        
+        void ProcessRequest(HttpRequestContext& context) override
+        {
+            std::ifstream fstream(fileName);
+            context.responseStream << fstream.rdbuf();
+        }
+    };
+
+    template <class Callable, class Controller>
+    class ApiEndPoint : public RouterEndPoint
+    {
+        Controller controller;
+        Callable   callable;
+    public:
+        ApiEndPoint(Controller controller, Callable callable)  :
+        controller(controller),
+        callable  (callable)
+        { }
+
+        void SendResponse(HttpRequestContext& context) override
+        { (controller->*callable)(context); }
+    };
+}
+
+
+class WebServer
+{
+protected:
+    template <class ValueT>
+    struct ClassId
+    { typedef ValueT ValueType; };
+
+    struct AddressId      : ClassId<std::string     > { };
+    struct PortId         : ClassId<unsigned short  > { };
+    struct ThreadsCountId : ClassId<unsigned int    > { };
+    struct RouterId       : ClassId<routing::Router*> { };
+public:
+    typedef webserverlibhelpers::Prop<AddressId     > Address;
+    typedef webserverlibhelpers::Prop<PortId        > Port;
+    typedef webserverlibhelpers::Prop<ThreadsCountId> Threads;
+
+    class Router
+    {
+        routing::Router* ptrRouter;
+    public:
+        Router(Router&& other)
+        { std::swap(ptrRouter, other.ptrRouter); }
+
+        Router(routing::Router* ptrRouter) : ptrRouter(ptrRouter) { }
+
+        operator routing::Router*() const
+        { return ptrRouter; }
+        ~Router() { delete ptrRouter; }
+    };
+
+protected:
+    typename Address    ::ValueType address;
+    typename Port       ::ValueType port;
+    typename Threads    ::ValueType threads;
+    routing::Router*                ptrRouter;
+
+public:
+    std::queue<tcp::socket> _acceptedConnectionsQueue;
+    std::mutex _connectionsQueueMutex;
+
+    void ListenLoop()
+    {
+        try
+        {
+            asio::io_context io_context(1);
+
+            tcp::acceptor acceptor{io_context, {ip::make_address(address), port}};
+            acceptor.non_blocking(true);
+
+            while (true)
+            {
+                tcp::socket socket{io_context};
+                boost::system::error_code error_code;
+                
+                while (acceptor.accept(socket, error_code) == asio::error::would_block)
+                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+                {
+                    std::lock_guard<std::mutex> lock_guard(_connectionsQueueMutex);
+                    _acceptedConnectionsQueue.push(std::move(socket));
+                }
+            }
+        }
+        catch (const std::exception &exception)
+        {
+            std::cerr << exception.what();
+        }
+    }
+
+    bool AcceptedConnectionsQueueEmpty()
+    {
+        std::lock_guard<std::mutex> lock_guard(_connectionsQueueMutex);
+        return _acceptedConnectionsQueue.empty();
+    }
+
+    tcp::socket PopAcceptedConnection()
+    {
+        while (true)
+        {
+            while (AcceptedConnectionsQueueEmpty())
+                std::this_thread::sleep_for(std::chrono::microseconds(5));
+            
+            std::lock_guard<std::mutex> lock_guard(_connectionsQueueMutex);
+            if (!_acceptedConnectionsQueue.empty())
+            {
+                struct Poper
+                {
+                    std::queue<tcp::socket> &_queue;
+                    Poper(std::queue<tcp::socket> &queue) : _queue(queue) {}
+                    ~Poper() { _queue.pop(); }
+                } poper(_acceptedConnectionsQueue);
+                return (std::move(_acceptedConnectionsQueue.front()));
+            }
+        }
+    }
+
+    void RequestProcessorLoop()
+    {
+        while (true)
         {
             try
             {
-                ip::address address = ip::make_address(_address);
-                asio::io_context io_context(1);
+                tcp::socket socket = PopAcceptedConnection();
+                Request request;
+                beast::flat_buffer buffer(8192);
 
-                tcp::acceptor acceptor{io_context, {address, _port}};
-                acceptor.non_blocking(true);
+                http::read(
+                    socket,
+                    buffer,
+                    request);
 
-                while (true)
-                {
-                    tcp::socket socket{io_context};
-                    boost::system::error_code error_code;
-                    
-                    while (acceptor.accept(socket, error_code) == asio::error::would_block)
-                        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+                Response response;
+                response.version(request.version());
 
-                    {
-                        std::lock_guard<std::mutex> lock_guard(_connectionsQueueMutex);
-                        _acceptedConnectionsQueue.push(std::move(socket));
-                    }
-                }
-            }
-            catch (const std::exception &exception)
-            {
-                std::cerr << exception.what();
-            }
-        }
-
-        bool AcceptedConnectionsQueueEmpty()
-        {
-            std::lock_guard<std::mutex> lock_guard(_connectionsQueueMutex);
-            return _acceptedConnectionsQueue.empty();
-        }
-
-        tcp::socket PopAcceptedConnection()
-        {
-            while (true)
-            {
-                while (AcceptedConnectionsQueueEmpty())
-                    std::this_thread::sleep_for(std::chrono::microseconds(5));
+                HttpRequestContext httpRequestContext(std::move(request));
                 
-                std::lock_guard<std::mutex> lock_guard(_connectionsQueueMutex);
-                if (!_acceptedConnectionsQueue.empty())
-                {
-                    struct Poper
-                    {
-                        std::queue<tcp::socket> &_queue;
-                        Poper(std::queue<tcp::socket> &queue) : _queue(queue) {}
-                        ~Poper() { _queue.pop(); }
-                    } poper(_acceptedConnectionsQueue);
-                    return (std::move(_acceptedConnectionsQueue.front()));
-                }
-            }
-        }
-
-        void RequestProcessorLoop()
-        {
-            while (true)
-            {
                 try
                 {
-                    tcp::socket socket = PopAcceptedConnection();
-                    Request request;
-                    beast::flat_buffer buffer(8192);
-
-                    http::read(
-                        socket,
-                        buffer,
-                        request);
-
-                    Response response;
-                    response.version(request.version());
-
-                    HttpRequestContext httpRequestContext(std::move(request));
-                    
-                    _requestProc(httpRequestContext);
-
-                    beast::ostream(response.body()) << httpRequestContext.responseStream.str();
-
-                    http::write(
-                        socket,
-                        response);
-
-                    socket.shutdown(tcp::socket::shutdown_send);
+                    ptrRouter->GetEndPoint(httpRequestContext).ProcessRequest(httpRequestContext);
                 }
-                catch (const std::exception exception)
+                catch(const std::exception exception)
                 {
-                    std::cerr << "processing request error: " << exception.what() << std::endl;
+                    std::cerr << "request process error: " << exception.what() << std::endl;
                 }
+
+
+                beast::ostream(response.body()) << httpRequestContext.responseStream.str();
+
+                http::write(
+                    socket,
+                    response);
+
+                socket.shutdown(tcp::socket::shutdown_send);
+            }
+            catch (const std::exception exception)
+            {
+                std::cerr << "request process error: " << exception.what() << std::endl;
             }
         }
+    }
+public:
+    template <class ... Args>
+    WebServer(const Args& ... args) :
+    address    (Address(args ..., Address("0.0.0.0")).value),
+    port       (Port   (args ..., Port          (80)).value),
+    threads    (Threads(args ..., Threads        (1)).value),
+    ptrRouter  (std::move(webserverlibhelpers::Selector<Router, Args ..., Router>::Result(args ..., Router(new routing::RouterNode {{"", new routing::StaticDocumentEndPoint("index.html")}}))))
+    {
+        ptrRouter = ptrRouter ? ptrRouter : ;
+        std::cout << "address : " << address << std::endl;
+        std::cout << "port    : " << port    << std::endl;
+        std::cout << "threads : " << threads << std::endl;
+        
+        std::thread(&WebServer::ListenLoop, this).detach();
+        for (unsigned int i = 0; i < threads; i++)
+            std::thread(&WebServer::RequestProcessorLoop, this).detach();
+    }
 
-    public:
-        template <class ... Args>
-        WebServer(const Args& ... args) :
-        _address    (Address(args ..., Address("0.0.0.0"))._value),
-        _port       (Port   (args ..., Port          (80))._value),
-        _threads    (Threads(args ..., Threads        (1))._value),
-        _requestProc(RequestProc(args ...)._value)
-        {
-            std::cout << "address : " << _address << std::endl;
-            std::cout << "port    : " << _port    << std::endl;
-            std::cout << "threads : " << _threads << std::endl;
-            
-            std::thread(&WebServer::ListenLoop, this).detach();
-            for (unsigned int i = 0; i < _threads; i++)
-                std::thread(&WebServer::RequestProcessorLoop, this).detach();
-        }
-    };
+    virtual ~WebServer()
+    { delete ptrRouter; }
 };
